50bbf71eaa856d7e586f15472c589334cd66ed52
nikitaantsev <71016018+nikitaantsev@users.noreply.github.com>
2023-03-28 21:56:33 +0300

    Add files via upload

---
+++ t_lab.5/t_lab.5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace t_lab._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            long n = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine(fact(n));
+            Console.ReadLine();
+        }
+        static BigInteger fact(BigInteger num)
+        {
+            return (num == 0) ? 1 : num * fact(num - 1);
+        }
+
+    }
+
+}
